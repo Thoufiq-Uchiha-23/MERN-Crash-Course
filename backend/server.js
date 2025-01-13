919d2417ce1a64bddf20339009a2a2ce7f90807e
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-13 23:04:39 +0530

    server backend

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // allow us to accept json data in req.body
 
 app.use("/api/products", productRoutes)
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
