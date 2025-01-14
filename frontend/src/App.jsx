b68220bb29b13b11680e182e8d1af938d8e843e1
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-14 21:34:58 +0530

    app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import { Box } from "@chakra-ui/react"
-import Navbar from "./components/Navbar"
-import HomePage from "./components/pages/HomePage"
-import CreatePage from "./components/pages/CreatePage"
-import { Route, Routes } from "react-router-dom"
+import { Box } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/pages/HomePage";
+import CreatePage from "./components/pages/CreatePage";
+import { Route, Routes } from "react-router-dom";
+import { useColorModeValue } from "@/components/ui/color-mode";
+// import { useProductStore } from "./store/product";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
